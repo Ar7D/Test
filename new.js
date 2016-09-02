4ab22a8bff6c63fbcd92d045913f7d41f929b277
Ar7D <Carraigdillon49@googlemail.com>
2016-09-02 18:24:36 +0100

    Update new.js

--- new.js
+++ new.js
@@ -1,1 +1,27 @@
+button = document.getElementsByTagName('button');
+para = document.getElementsByTagName('div');
 
+function show(x) {
+    para[x].style.display = 'block';
+    button[x].onclick = function() {
+        hide(x);
+    }
+}
+
+function hide(x) {
+    para[x].style.display = 'none';
+    button[x].onclick = function() {
+        show(x);
+    }
+}
+
+button[0].onclick = function() {
+    show(0);
+}
+
+button[1].onclick = function() {
+    show(1);
+}
+button[2].onclick = function() {
+    show(2);
+}
