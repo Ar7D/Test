5fcc9947baa2a3261ee83ba460d3b45e9366d8a1
Ar7D <Carraigdillon49@googlemail.com>
2016-09-02 17:09:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 var names = ["Mathew Jones", "Katie Holmes", "Charlotte Wright","James Wilks","Sophie Broom"]; 
 
-document.getElementById("demo").innerHTML =[2]
+//document.getElementById("demo").innerHTML = names [2]
+
+
+
+button = document.getElementsByTagName('button');
+para = document.getElementsByTagName('names');
+
+function show(x) {
+    para[x].style.display = 'block';
+    button[x].onclick = function() {
+        hide(x);
+    }
+}
+
+function hide(x) {
+    para[x].style.display = 'none';
+    button[x].onclick = function() {
+        show(x);
+    }
+}
+
+button[0].onclick = function() {
+    show(0);
+}
+
+button[1].onclick = function() {
+    show(1);
+}
+button[2].onclick = function() {
+    show(2);
+}
+
 
 /*
 function myFunction1() {
